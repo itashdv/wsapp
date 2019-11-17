c8e38051b48cb9f7cbd766c0d7fda5e0000aaf78
itash <itashcb@gmail.com>
2019-11-17 13:02:22 +0700

    Backup commit 17.11.2019.

--- src/components/company/MenuCategory.js
+++ src/components/company/MenuCategory.js
@@ -8,10 +8,18 @@ const StyledMenuCategory = styled.div`
 const MenuCategory = (props) => {
     const [category, setCategory] = useState(props.category);
     const [items, setItems] = useState(null);
+    const fetchItems = async (categoryId) => {
+    	const response = await fetch(`https://mighty-brook-28904.herokuapp.com/api/items/${ categoryId }`);
+    	const result = await response.json();
+    	console.log(result);
+    	setItems(result);
+    }
+    useEffect(() => {
+    	fetchItems(category._id);
+    }, [category.id]);
     return (
         <StyledMenuCategory>
             <h5>{ category.name }</h5>
-
         </StyledMenuCategory>
     );
 }
